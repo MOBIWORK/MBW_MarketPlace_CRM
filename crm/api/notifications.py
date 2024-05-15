2ac2465dfc421455ff27936d3469b04fcd71a22e
dev3.ekgis <dev3.ekgis@gmail.com>
2024-05-15 17:09:30 +0700

    build layout notifications

--- crm/api/notifications.py
+++ crm/api/notifications.py
@@ -3,7 +3,7 @@ from frappe.query_builder import Order
 
 
 @frappe.whitelist()
-def get_notifications():
+def get_notifications(type_notify=None):
     Notification = frappe.qb.DocType("CRM Notification")
     query = (
         frappe.qb.from_(Notification)
@@ -11,12 +11,31 @@ def get_notifications():
         .where(Notification.to_user == frappe.session.user)
         .orderby("creation", order=Order.desc)
     )
+    if type_notify is not None:
+        if type_notify == "Comment":
+            query = query.where(
+                (Notification.type == 'Mention') |
+                (Notification.type == 'RelyComment')
+            )
+        elif type_notify == "Task":
+            query = query.where(Notification.type == 'Task')
+        elif type_notify == "Remind":
+            query = query.where(Notification.type == 'Remind')
     notifications = query.run(as_dict=True)
 
     _notifications = []
     for notification in notifications:
+        reference_doctype = "deal"
+        route_name = "Deal"
+        if notification.reference_doctype == "CRM Lead":
+            reference_doctype = "lead"
+            route_name = "Lead"
+        elif notification.reference_doctype == "Contact":
+            reference_doctype = "contact"
+            route_name = "Contact"
         _notifications.append(
             {
+                "name": notification.name,
                 "creation": notification.creation,
                 "from_user": {
                     "name": notification.from_user,
@@ -31,13 +50,9 @@ def get_notifications():
                 "notification_text": notification.notification_text,
                 "notification_type_doctype": notification.notification_type_doctype,
                 "notification_type_doc": notification.notification_type_doc,
-                "reference_doctype": (
-                    "deal" if notification.reference_doctype == "CRM Deal" else "lead"
-                ),
+                "reference_doctype": reference_doctype,
                 "reference_name": notification.reference_name,
-                "route_name": (
-                    "Deal" if notification.reference_doctype == "CRM Deal" else "Lead"
-                ),
+                "route_name": route_name
             }
         )
 
@@ -48,12 +63,16 @@ def get_notifications():
 def mark_as_read(user=None, doc=None):
     user = user or frappe.session.user
     filters = {"to_user": user, "read": False}
+    notifications = []
     if doc:
         or_filters = [
-            {"comment": doc},
+            {"name": doc},
             {"notification_type_doc": doc},
         ]
-    for n in frappe.get_all("CRM Notification", filters=filters, or_filters=or_filters):
+        notifications = frappe.get_all("CRM Notification", filters=filters, or_filters=or_filters)
+    else:
+        notifications = frappe.get_all("CRM Notification", filters=filters)
+    for n in notifications:
         d = frappe.get_doc("CRM Notification", n.name)
         d.read = True
         d.save()
--- frontend/src/components/Notifications.vue
+++ frontend/src/components/Notifications.vue
@@ -39,16 +39,23 @@
           </Tooltip>
         </div>
       </div>
+      <div style="display: flex;">
+        <div :class="['btn-filter-notify', { 'btn-filter-notify-active': notify_type === 'all' }]" @click="chang_type_notify('all')">Tất cả</div>
+        <div :class="['btn-filter-notify', { 'btn-filter-notify-active': notify_type === 'Comment' }]" @click="chang_type_notify('Comment')">Comment</div>
+        <div :class="['btn-filter-notify', { 'btn-filter-notify-active': notify_type === 'Task' }]" @click="chang_type_notify('Task')">Công việc</div>
+        <div :class="['btn-filter-notify', { 'btn-filter-notify-active': notify_type === 'Remind' }]" @click="chang_type_notify('Remind')">Nhắc nhở</div>
+      </div>
       <div
         v-if="notificationsStore().allNotifications?.length"
         class="divide-y overflow-auto text-base"
       >
+      <div v-for="n in notificationsStore().allNotifications">
         <RouterLink
-          v-for="n in notificationsStore().allNotifications"
-          :key="n.comment"
+          v-if="n.reference_name != ''"
+          :key="n.name"
           :to="getRoute(n)"
           class="flex cursor-pointer items-start gap-2.5 px-4 py-2.5 hover:bg-gray-100"
-          @click="mark_as_read(n.comment || n.notification_type_doc)"
+          @click="mark_as_read(n.name || n.notification_type_doc)"
         >
           <div class="mt-1 flex items-center gap-2.5">
             <div
@@ -79,6 +86,40 @@
             </div>
           </div>
         </RouterLink>
+        <div v-else
+          class="flex cursor-pointer items-start gap-2.5 px-4 py-2.5 hover:bg-gray-100"
+          @click="mark_as_read(n.name || n.notification_type_doc)">
+          <div class="mt-1 flex items-center gap-2.5">
+            <div
+              class="h-[5px] w-[5px] rounded-full"
+              :class="[n.read ? 'bg-transparent' : 'bg-gray-900']"
+            />
+            <WhatsAppIcon
+              v-if="n.type == 'WhatsApp'"
+              class="size-7 rounded-full"
+            />
+            <UserAvatar v-else :user="n.from_user.name" size="lg" />
+          </div>
+          <div>
+            <div v-if="n.notification_text" v-html="n.notification_text" />
+            <div v-else class="mb-2 space-x-1 leading-5 text-gray-600">
+              <span class="font-medium text-gray-900">
+                {{ n.from_user.full_name }}
+              </span>
+              <span>
+                {{ __('mentioned you in {0}', [n.reference_doctype]) }}
+              </span>
+              <span class="font-medium text-gray-900">
+                {{ n.reference_name }}
+              </span>
+            </div>
+            <div class="text-sm text-gray-600">
+              {{ __(timeAgo(n.creation)) }}
+            </div>
+          </div>
+        </div>
+      </div>
+      
       </div>
       <div
         v-else
@@ -107,6 +148,8 @@ import { ref, onMounted, onBeforeUnmount } from 'vue'
 const { $socket } = globalStore()
 
 const target = ref(null)
+const notify_type = ref("all")
+
 onClickOutside(
   target,
   () => {
@@ -119,11 +162,22 @@ onClickOutside(
   }
 )
 
+function chang_type_notify(type){
+  notify_type.value = type;
+  if(type == "all"){
+    notificationsStore().notifications.params = {};
+  }else{
+    notificationsStore().notifications.params = {type_notify: type};
+  }
+  notificationsStore().notifications.reload();
+}
+
 function toggleNotificationPanel() {
   notificationsStore().toggle()
 }
 
 function mark_as_read(doc) {
+  console.log(doc)
   notificationsStore().mark_doc_as_read(doc)
 }
 
@@ -145,13 +199,38 @@ function getRoute(notification) {
     params = {
       dealId: notification.reference_name,
     }
+  }else if(notification.route_name === 'Contact'){
+    params = {
+      contactId: notification.reference_name
+    }
   }
   return {
     name: notification.route_name,
     params: params,
-    hash: '#' + notification.comment || notification.notification_type_doc,
+    hash: '#' + notification.notification_type_doc,
   }
 }
 
 onMounted(() => {})
 </script>
+<style scoped>
+  .btn-filter-notify{
+    background-color: #f4f4f4 !important;
+    color: #444 !important;
+    border-color: #ddd !important;
+    font-size: 13px;
+    margin-left: 5px;
+    border-radius: 15px;
+    margin: 10px 5px;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+  .btn-filter-notify-active{
+    background-color: #baebe1 !important;
+    color: #1876f2 !important;
+    border-color: #baebe1 !important;
+    box-shadow: none !important;
+    font-weight: 500;
+    outline: none !important;
+  }
+</style>
