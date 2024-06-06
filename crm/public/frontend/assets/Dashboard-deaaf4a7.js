5ac6dfd3d27994af949fc739d82a6bf66d3404bb
dev3.ekgis <dev3.ekgis@gmail.com>
2024-06-06 17:04:28 +0700

    da ngon ngu

---
+++ crm/public/frontend/assets/Dashboard-deaaf4a7.js
@@ -0,0 +1,2 @@
+import{_ as e,a as t}from"./LayoutHeader-25838069.js";import{e as r,l as s,i as o,h as _,j as c}from"./index-feedfb27.js";const d={__name:"Dashboard",setup(l){const a=[{label:"Dashboard",route:{name:"Dashboard"}}];return(m,i)=>(r(),s(e,null,{"left-header":o(()=>[_(c(t),{items:a})]),_:1}))}};export{d as default};
+//# sourceMappingURL=Dashboard-deaaf4a7.js.map
--- crm/public/frontend/assets/organizations-8b547f80.js
+++
@@ -1,2 +0,0 @@
-import{b9 as i,z as e,B as s}from"./index-f37caafa.js";const u=i("crm-organizations",()=>{let r=e({});const a=s({url:"crm.api.session.get_organizations",cache:"organizations",initialData:[],auto:!0,transform(o){for(let t of o)r[t.name]=t;return o},onError(o){o&&o.exc_type==="AuthenticationError"&&router.push("/login")}});function n(o){return r[o]}return{organizations:a,getOrganization:n}});export{u as o};
-//# sourceMappingURL=organizations-8b547f80.js.map
