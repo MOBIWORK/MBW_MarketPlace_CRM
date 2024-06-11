577800ec1ae0750c0b595f6800c18f67f125c09f
dev3.ekgis <dev3.ekgis@gmail.com>
2024-06-11 08:15:05 +0700

    build app

--- crm/public/frontend/assets/Dashboard-0037f768.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,a as t}from"./LayoutHeader-15724b77.js";import{e as r,l as s,i as o,h as _,j as c}from"./index-a251e902.js";const d={__name:"Dashboard",setup(l){const a=[{label:"Dashboard",route:{name:"Dashboard"}}];return(m,i)=>(r(),s(e,null,{"left-header":o(()=>[_(c(t),{items:a})]),_:1}))}};export{d as default};
-//# sourceMappingURL=Dashboard-0037f768.js.map
---
+++ crm/public/frontend/assets/Dashboard-a160e194.js
@@ -0,0 +1,2 @@
+import{_ as e,a as t}from"./LayoutHeader-3b315ab2.js";import{e as r,l as s,i as o,h as _,j as c}from"./index-feace97c.js";const d={__name:"Dashboard",setup(l){const a=[{label:"Dashboard",route:{name:"Dashboard"}}];return(m,i)=>(r(),s(e,null,{"left-header":o(()=>[_(c(t),{items:a})]),_:1}))}};export{d as default};
+//# sourceMappingURL=Dashboard-a160e194.js.map
