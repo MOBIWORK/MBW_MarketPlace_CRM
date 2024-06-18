b2bde7073a989cf37ae6ade8c4fda98e5d5e4622
dev3.ekgis <dev3.ekgis@gmail.com>
2024-06-18 08:37:44 +0700

    fix bug and build app

--- crm/public/frontend/assets/Dashboard-172c5b05.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,a as t}from"./LayoutHeader-13bffee4.js";import{e as r,l as s,i as o,h as _,j as c}from"./index-d8c5d38f.js";const d={__name:"Dashboard",setup(l){const a=[{label:"Dashboard",route:{name:"Dashboard"}}];return(m,i)=>(r(),s(e,null,{"left-header":o(()=>[_(c(t),{items:a})]),_:1}))}};export{d as default};
-//# sourceMappingURL=Dashboard-172c5b05.js.map
---
+++ crm/public/frontend/assets/Dashboard-1f800ec6.js
@@ -0,0 +1,2 @@
+import{_ as e,a as t}from"./LayoutHeader-ace7ae22.js";import{e as r,l as s,i as o,h as _,j as c}from"./index-e5a8141b.js";const d={__name:"Dashboard",setup(l){const a=[{label:"Dashboard",route:{name:"Dashboard"}}];return(m,i)=>(r(),s(e,null,{"left-header":o(()=>[_(c(t),{items:a})]),_:1}))}};export{d as default};
+//# sourceMappingURL=Dashboard-1f800ec6.js.map
