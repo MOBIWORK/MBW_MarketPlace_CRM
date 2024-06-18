ae26c24bb7ab8b4187e7001716d5327f8e7f67d4
dev3.ekgis <dev3.ekgis@gmail.com>
2024-06-18 13:36:54 +0700

    fix bug

---
+++ crm/public/frontend/assets/Dashboard-2e080f92.js
@@ -0,0 +1,2 @@
+import{_ as e,a as t}from"./LayoutHeader-2127a7fa.js";import{e as r,l as s,i as o,h as _,j as c}from"./index-9ba718b4.js";const d={__name:"Dashboard",setup(l){const a=[{label:"Dashboard",route:{name:"Dashboard"}}];return(m,i)=>(r(),s(e,null,{"left-header":o(()=>[_(c(t),{items:a})]),_:1}))}};export{d as default};
+//# sourceMappingURL=Dashboard-2e080f92.js.map
--- crm/public/frontend/assets/EmailTemplate-ebd872a5.js
+++
@@ -1,2 +0,0 @@
-import{_ as t,e as s,f as a,v as e}from"./index-e5a8141b.js";const o={},l=e("h1",null,"Email Templates",-1),c=e("p",null,"Here is a list of email templates",-1),n=[l,c];function _(r,i){return s(),a("div",null,n)}const d=t(o,[["render",_]]);export{d as default};
-//# sourceMappingURL=EmailTemplate-ebd872a5.js.map
