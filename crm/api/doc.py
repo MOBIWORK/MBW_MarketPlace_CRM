5c59d6c4e4434fd913b805182b225a97fdbc8eb0
dev3.ekgis <dev3.ekgis@gmail.com>
2024-06-25 14:53:19 +0700

    fix bug

--- crm/api/doc.py
+++ crm/api/doc.py
@@ -593,8 +593,8 @@ def convert_to_deal(lead):
 	lead.save(ignore_permissions=True)
 	contact = lead.create_contact(False)
 	organization = lead.create_organization()
-	deal = lead.create_deal(contact, organization)
-	return deal
+	#deal = lead.create_deal(contact, organization)
+	return "DEALSs"
 
 def read_content(content, extension):
 	error_title = _("Template Error")
